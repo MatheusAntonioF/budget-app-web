153cc16384b2c91688d471d00110c6bf4228861f
Matheus  Felipe Antonio <matheusantoniof@gmail.com>
2022-01-12 19:07:30 -0300

    [FEAT] Create currency input component

---
+++ src/components/Input/Datepicker/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import { Core } from '../styles';
+
+export const DatepickerCore = styled(Core)`
+  input {
+    width: 100%;
+    height: 100%;
+    outline: 0;
+    border: 0;
+
+    padding-left: 10px;
+  }
+`;
